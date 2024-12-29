b0cc09fd522f1311a6fbfc21efe53e8e30109edd
PonceGL <poncianogl@hotmail.com>
2024-12-29 17:14:31 -0600

    Initial Commit

---
+++ Features/FeaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeaturesApp.swift
+//  Features
+//
+//  Created by Ponciano Guevara Lozano on 29/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FeaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
