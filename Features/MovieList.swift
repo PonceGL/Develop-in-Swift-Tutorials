5d53acc82111fbdb3c72a1a2ceef9fdabe0e3d89
PonceGL <poncianogl@hotmail.com>
2024-12-29 19:38:08 -0600

    feat(MovieList): Improve data entry using a sheet

--- Features/MovieList.swift
+++ Features/MovieList.swift
@@ -11,6 +11,7 @@ import SwiftData
 struct MovieList: View {
     @Query(sort: \Movie.title) private var movies: [Movie]
     @Environment(\.modelContext) private var context
+    @State private var newMovie: Movie?
     
     var body: some View {
         NavigationSplitView {
@@ -32,6 +33,11 @@ struct MovieList: View {
                     EditButton()
                 }
             }
+            .sheet(item: $newMovie) { movie in
+                NavigationStack {
+                    MovieDetail(movie: movie)
+                }
+            }
         } detail: {
             Text("Select a movie")
                 .navigationTitle("Movie")
@@ -40,7 +46,9 @@ struct MovieList: View {
     }
     
     private func addMovie() {
-        context.insert(Movie(title: "New Movie", releaseDate: .now))
+        let newMovie = Movie(title: "", releaseDate: .now)
+        context.insert(newMovie)
+        self.newMovie = newMovie
     }
     
     private func deleteMovies(indexes: IndexSet) {
